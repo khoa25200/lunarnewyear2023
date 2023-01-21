0285b5aa09953d0124ac104cf66712b3235215cd
khoa25200 <91365201+khoa25200@users.noreply.github.com>
2023-01-21 21:44:06 +0700

    useDevice effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import Landingpage from "./pages/LandingPage";
 import AnimationCursorCustom from './components/AniamationCursorCustom/index';
+import { useDevice } from "react-use-device";
 function App() {
+  const { isMOBILE, isTABLET } = useDevice();
   return (
     <div className="App">
-      <Landingpage />
+      {
+        !isMOBILE && !isTABLET ? (
+          <Landingpage />
+        ) : <section className='noSupport'>
+          no support for mobile devices
+          <br />  
+          🥺Sorry
+          <br />
+          <br />
+          👇
+          <br />
+          <br />
+          Please use Laptop/PC to view my website!!!
+        </section>
+      }
       <AnimationCursorCustom />
     </div>
   );
